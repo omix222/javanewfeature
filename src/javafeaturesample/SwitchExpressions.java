ef497ce3b98c232c1b5b020f9b628707aa4bd36f
hiromi takhashi <omix222@users.noreply.github.com>
2019-03-20 16:20:40 +0900

    Update SwitchExpressions.java

--- src/javafeaturesample/SwitchExpressions.java
+++ src/javafeaturesample/SwitchExpressions.java
@@ -1,15 +1,21 @@
-enum Fruit {
-    Apple,
-    Melon,
-    Orange
-}
+package javafeaturesample;
 
-void call() {
-    Fruit fruit_type = Fruit.Melon;
-    int price = switch (fruit_type) {
-        case Apple,Orange -> 100;
-        case Melon -> 300;
-    };
-    System.out.print(price);
+import java.util.function.Function;
+
+public class SwitchExpressions {
+    enum Fruit {
+        Apple,
+        Melon,
+        Orange
+    }
+
+    void call() {
+        Fruit fruit_type = Fruit.Melon;
+        int price = switch (fruit_type) {
+            case Apple,Orange -> 100;
+            case Melon -> 300;
+        };
+        System.out.print(price);
+    }
 }
 
